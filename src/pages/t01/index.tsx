e8520b21935ff094d2f6bcc6459a6b8bbcf82751
harutaka <10650294+harutaka@users.noreply.github.com>
2021-12-13 18:30:02 +0900

    first commit

---
+++ src/pages/t01/index.tsx
@@ -0,0 +1,99 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/image"
+import Link from "next/link"
+
+const T01Index: NextPage = () => {
+  const imgs = ["/img/t01/bicycle1.jpg", "/img/t01/bicycle2.jpg", "/img/t01/bicycle3.jpg"]
+
+  return (
+    <>
+      <Head>
+        <title>Profile</title>
+      </Head>
+
+      <header
+        id="header"
+        className="max-w-[960px] mx-auto py-0 px-[4%] text-center flex justify-between items-center my-auto"
+      >
+        <h1 className="w-[120px] leading-none px-0 py-2.5">
+          <Link href="index.html">
+            <a>
+              <Image src="/img/t01/logo.svg" alt="Profile" width={120} height={40} />
+            </a>
+          </Link>
+        </h1>
+        <nav>
+          <ul className="flex px-2.5 py-0">
+            <li className="ml-[30px]">
+              <Link href="#about">
+                <a className="text-[#24292e] hover:opacity-70">About</a>
+              </Link>
+            </li>
+            <li className="ml-[30px]">
+              <Link href="#bicycle">
+                <a className="text-[#24292e] hover:opacity-70">Bicycle</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <div id="mainvisual" className="mb-[80px]">
+          <Image src="/img/t01/mainvisual.jpg" alt="テキスト" width={1920} height={600} className="object-cover" />
+        </div>
+
+        <section id="about" className="max-w-[960px] mt-0 mx-auto mb-[100px] py-0 px-[4%] text-center">
+          <h2 className="inline-block text-[2rem] text-center mb-[60px] border-b-solid border-b border-[#383e45] font-bold">
+            About
+          </h2>
+          <div className="flex justify-center items-center">
+            <div className="mr-[30px]">
+              <Image
+                src="/img/t01/about.jpg"
+                className="rounded-[50%]"
+                width={100}
+                height={100}
+                alt="テキストテキストテキスト"
+              />
+            </div>
+            <div className="text-left">
+              <h3 className="text-base my-2.5 mx-0 font-bold">KAKERU MIYAICHI</h3>
+              <p>
+                テキストテキストテキストテキストテキストテキストテキスト
+                <br />
+                テキストテキストテキストテキストテキストテキストテキスト
+                <br />
+                テキストテキストテキストテキストテキストテキストテキスト
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section id="bicycle" className="max-w-[960px] mt-0 mx-auto mb-[100px] py-0 px-[4%] text-center">
+          <h2 className="inline-block text-[2rem] text-center mb-[60px] border-b-solid border-b border-[#383e45] font-bold">
+            Bicycle
+          </h2>
+          <ul className="flex justify-between">
+            {imgs.map((item) => {
+              return (
+                <li className="w-[32%]" key={item}>
+                  <Image src={item} alt="テキストテキストテキスト" width={640} height={424} />
+                  <h3 className="text-base my-2.5 mx-0 font-bold">タイトルタイトル</h3>
+                  <p>テキストテキストテキスト</p>
+                </li>
+              )
+            })}
+          </ul>
+        </section>
+      </main>
+
+      <footer id="footer" className="text-[0.5rem] py-2.5 px-0 text-center">
+        <p>&copy; 2020 Profile</p>
+      </footer>
+    </>
+  )
+}
+
+export default T01Index
