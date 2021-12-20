89ee93ffa5dce4ec885610794b8ce5cb93b20e79
harutaka <10650294+harutaka@users.noreply.github.com>
2021-12-20 11:04:45 +0900

    全体要素修正

--- src/pages/t02/index.tsx
+++ src/pages/t02/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 const T02Index: NextPage = () => {
   return (
-    <>
+    <div className="min-h-screen font-meiryo bg-blue-50">
       <Head>
         <title>PHOTO BOOK</title>
         <meta name="description" content="フォトサイト練習ページ" />
@@ -14,68 +14,66 @@ const T02Index: NextPage = () => {
         <meta name="twitter:card" content="summary" />
       </Head>
 
-      <body className="font-meiryo bg-blue-50">
-        <main className="mx-auto max-w-[1000px]">
-          <h1 className="my-2 mt-12 text-2xl font-bold">PHOTO BOOK</h1>
-          <Image src="/img/t02/mainvisual.jpg" alt="main" width={1000} height={400} />
+      <main className="mx-auto max-w-[1000px]">
+        <h1 className="pt-12 pb-2 text-2xl font-bold">PHOTO BOOK</h1>
+        <Image src="/img/t02/mainvisual.jpg" alt="main" width={1000} height={400} />
 
-          {/* <!-- INDEX --> */}
-          <section className="mt-16 bg-white">
-            <div className="mx-auto w-[600px]">
-              <h2 className="pt-8 my-2 text-lg font-bold">INDEX</h2>
-              <ol className="pb-6 list-decimal">
-                <li className="mx-4 text-sm leading-10">
-                  タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
-                </li>
-                <li className="mx-4 text-sm leading-10">
-                  タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
-                </li>
-                <li className="mx-4 text-sm leading-10">
-                  タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
-                </li>
-                <li className="mx-4 text-sm leading-10">
-                  タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
-                </li>
-                <li className="mx-4 text-sm leading-10">
-                  タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
-                </li>
-              </ol>
+        {/* <!-- INDEX --> */}
+        <section className="mt-16 bg-white">
+          <div className="mx-auto w-[600px]">
+            <h2 className="pt-8 my-2 text-lg font-bold">INDEX</h2>
+            <ol className="pb-6 list-decimal">
+              <li className="mx-4 text-sm leading-10">
+                タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+              </li>
+              <li className="mx-4 text-sm leading-10">
+                タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+              </li>
+              <li className="mx-4 text-sm leading-10">
+                タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+              </li>
+              <li className="mx-4 text-sm leading-10">
+                タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+              </li>
+              <li className="mx-4 text-sm leading-10">
+                タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
+              </li>
+            </ol>
+          </div>
+        </section>
+
+        {/* <!-- DETAIL --> */}
+        <section className="mx-auto mt-16 mb-28 w-[600px]">
+          <h2 className="mb-4 text-lg font-bold">DETAIL</h2>
+          <div className="flex">
+            <div className="pr-5 w-1/2">
+              <Image src="/img/t02/detail.jpg" alt="detail" width={270} height={270} />
             </div>
-          </section>
-
-          {/* <!-- DETAIL --> */}
-          <section className="mx-auto mt-16 mb-28 w-[600px]">
-            <h2 className="mb-4 text-lg font-bold">DETAIL</h2>
-            <div className="flex">
-              <div className="pr-5 w-1/2">
-                <Image src="/img/t02/detail.jpg" alt="detail" width={270} height={270} />
+            <div className="ml-5 w-1/2">
+              <h3 className="mb-4 text-lg font-bold">タイトルタイトルタイトル</h3>
+              <hr className="border-gray-300" />
+              <div className="grid grid-cols-4 my-4">
+                <div className="text-sm">著者</div>
+                <div className="col-span-3 text-sm">タイトルタイトルタイトル</div>
+                <div className="text-sm">出版社</div>
+                <div className="col-span-3 text-sm">タイトルタイトルタイトル</div>
+                <div className="text-sm">発行年</div>
+                <div className="col-span-3 text-sm">タイトルタイトルタイトル</div>
               </div>
-              <div className="ml-5 w-1/2">
-                <h3 className="mb-4 text-lg font-bold">タイトルタイトルタイトル</h3>
-                <hr className="border-gray-300" />
-                <div className="grid grid-cols-4 my-4">
-                  <div className="text-sm">著者</div>
-                  <div className="col-span-3 text-sm">タイトルタイトルタイトル</div>
-                  <div className="text-sm">出版社</div>
-                  <div className="col-span-3 text-sm">タイトルタイトルタイトル</div>
-                  <div className="text-sm">発行年</div>
-                  <div className="col-span-3 text-sm">タイトルタイトルタイトル</div>
-                </div>
-                <hr className="border-gray-300" />
-                <p className="my-4 text-sm">
-                  テキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキスト
-                </p>
-                <a href="#" className="underline">
-                  オンラインストアで見る
-                </a>
-              </div>
+              <hr className="border-gray-300" />
+              <p className="my-4 text-sm">
+                テキストテキストテキストテキストテキストテキスト テキストテキストテキストテキストテキストテキスト
+              </p>
+              <a href="#" className="underline">
+                オンラインストアで見る
+              </a>
             </div>
-          </section>
+          </div>
+        </section>
 
-          <footer className="my-4 mx-auto w-[600px] text-[10px]">© 2020 PHOTO BOOK</footer>
-        </main>
-      </body>
-    </>
+        <footer className="py-4 mx-auto w-[600px] text-[10px]">© 2020 PHOTO BOOK</footer>
+      </main>
+    </div>
   )
 }
 
--- src/pages/t04/index.tsx
+++ src/pages/t04/index.tsx
@@ -12,7 +12,7 @@ const T04Index: NextPage = () => {
   ]
 
   return (
-    <>
+    <div className="bg-gray-100">
       <Head>
         <title>Recipe Diary</title>
         <meta name="description" content="レシピサイト練習ページ" />
@@ -23,52 +23,50 @@ const T04Index: NextPage = () => {
         <link rel="icon" href="/img/t04/favicon.ico" />
       </Head>
 
-      <div className="bg-gray-100">
-        <main>
-          <div className="object-cover relative mb-20 w-full h-screen">
-            <Image src="/img/t04/mainvisual.jpg" alt="" layout="fill" objectFit="cover" />
-          </div>
+      <main>
+        <div className="object-cover relative mb-20 w-full h-screen">
+          <Image src="/img/t04/mainvisual.jpg" alt="" layout="fill" objectFit="cover" />
+        </div>
 
-          <div className="py-0 px-5 text-center">
-            <h1 className="mb-5 text-3xl font-bold">Recipe Diary</h1>
-            <p>
-              日々の料理レシピをまとめています。
-              <br />
-              和食や洋食、中華、お菓子までいろいろな料理レシピをアップしていますので、
-              <br />
-              みなさんの献立にお役立てくださいね！
-            </p>
-          </div>
+        <div className="py-0 px-5 text-center">
+          <h1 className="mb-5 text-3xl font-bold">Recipe Diary</h1>
+          <p>
+            日々の料理レシピをまとめています。
+            <br />
+            和食や洋食、中華、お菓子までいろいろな料理レシピをアップしていますので、
+            <br />
+            みなさんの献立にお役立てくださいね！
+          </p>
+        </div>
 
-          <ul className="md:flex my-[60px]">
-            {imgArr.map((item, i) => (
-              <li className="relative md:w-1/3 h-[500px]" key={i}>
-                <Image src={item} alt="" layout="fill" objectFit="cover" />
+        <ul className="md:flex my-[60px]">
+          {imgArr.map((item, i) => (
+            <li className="relative md:w-1/3 h-[500px]" key={i}>
+              <Image src={item} alt="" layout="fill" objectFit="cover" />
+            </li>
+          ))}
+        </ul>
+
+        <div className="py-0 px-5 mb-20 text-center">
+          <Link href="#">
+            <a className="inline-block py-3 px-14 text-sm no-underline border border-gray-800">レシピ一覧を見る</a>
+          </Link>
+        </div>
+
+        <footer className="p-5 text-xs text-center">
+          <ul className="flex justify-center mb-5">
+            {snsArr.map((item, i) => (
+              <li key={i} className="my-0 mx-2.5 underline">
+                <Link href={item.href}>
+                  <a>{item.name}</a>
+                </Link>
               </li>
             ))}
           </ul>
-
-          <div className="py-0 px-5 mb-20 text-center">
-            <Link href="#">
-              <a className="inline-block py-3 px-14 text-sm no-underline border border-gray-800">レシピ一覧を見る</a>
-            </Link>
-          </div>
-
-          <footer className="p-5 text-xs text-center">
-            <ul className="flex justify-center mb-5">
-              {snsArr.map((item, i) => (
-                <li key={i} className="my-0 mx-2.5 underline">
-                  <Link href={item.href}>
-                    <a>{item.name}</a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-            <p>&copy; 2021 Recipe Diary</p>
-          </footer>
-        </main>
-      </div>
-    </>
+          <p>&copy; 2021 Recipe Diary</p>
+        </footer>
+      </main>
+    </div>
   )
 }
 
